3e8ffb937cee6242a0cede3bd53ab69d0039f62a
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-01-29 22:46:33 -0300

    add change size button

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const container = document.querySelector('.container')
-const area = 16
 
-for (let i = 0; i < area * area; i++) {
-  let square = document.createElement('div')
-  square.className = 'square'
-  square.style.flex = `1 1 ${500 / (area + 1)}px`
-  square.style.height = `${500 / (area + 1)}px`
-  container.appendChild(square)
+function squareHovering(e) {
+  this.style.backgroundColor = 'black'
 }
+
+const createGrid = (area = 16) => {
+  for (let i = 0; i < area * area; i++) {
+    let square = document.createElement('div')
+    square.className = 'square'
+    square.style.width = `${100 / area}%`
+    square.style.height = `${100 / area}% `
+    square.addEventListener('mouseenter', squareHovering)
+    container.appendChild(square)
+  }
+}
+
+createGrid()
+
+function getSize() {
+  let area = Number(prompt('Enter a size lower that 100'))
+
+  if (area > 0 && area < 100) {
+    container.innerHTML = ''
+    createGrid(area)
+  } else {
+    alert('Size must be a number greater than 0 and lower than 100')
+  }
+}
+
+const changeSizeBtn = document.querySelector('#changeSizeBtn')
+
+changeSizeBtn.addEventListener('click', getSize)
